7453654dd939861f2dfbb2264f2f8e761fef97af
dilyanabodurova <diliana.bodurova@abv.bg>
2025-12-22 19:11:08 +0200

    nato alphabet

--- main.py
+++ main.py
@@ -20,8 +20,15 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+nato_alphabet_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
+#TODO 1. Create a dictionary in this format:
+nato_alphabet_dict= {raw.letter:raw.code for (index, raw) in nato_alphabet_df.iterrows()}
+
+user_word = input("Enter a word: ").upper()
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
+user_letters = {nato_alphabet_dict[letter] for letter in user_word}
+
+
+print(user_letters)
